d414f5479f161a83f3cdf7bd4814c7e2dfa0927a
Norikazu Teraguchi <norimaki.tera@gmail.com>
2017-03-03 00:15:35 +0900

    GetComponentを使ってみる

---
+++ Assets/GameDirector.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; //UI部品を使うために必要
+
+public class GameDirector : MonoBehaviour
+{
+    private GameObject car;
+    private GameObject flag;
+    private GameObject distance;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    this.car = GameObject.Find("car");
+	    this.flag = GameObject.Find("flag");
+	    this.distance = GameObject.Find("Distance");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    float length = this.flag.transform.position.x - this.car.transform.position.x;
+	    this.distance.GetComponent<Text>().text = "ゴールまで" + length.ToString("F2") + "m";
+	}
+}
