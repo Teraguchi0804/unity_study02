addf15ce712b6fe36e126d8adcf0a5a0bda718f9
Norikazu Teraguchi <norimaki.tera@gmail.com>
2017-03-02 23:25:05 +0900

    init_comit

---
+++ Assets/CarController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    private float speed = 0;
+    private Vector2 startPos;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    //スワイプの長さを求める
+	    if (Input.GetMouseButtonDown(0)) //マウスがクリックされたら
+	    {
+	        //マウスをクリックした座標
+	        this.startPos = Input.mousePosition;
+	    }
+	    else if(Input.GetMouseButton(0))
+	    {
+	        //マウスを離した座標
+	        Vector2 endPos = Input.mousePosition;
+	        float swipeLength = (endPos.x - this.startPos.x);
+
+	        //スワイプの長さを初速度に変換する
+	        this.speed = swipeLength / 800.0f;
+	    }
+
+	    transform.Translate(this.speed, 0, 0); //移動
+	    this.speed *= 0.98f;                   //減速
+	}
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# Particle Paper
+
+EditorはJetBrains Riderを使用
+UnityのVersionは5.5.0f3
+
+参考：http://tsubakit1.hateblo.jp/entry/2015/09/04/233000
